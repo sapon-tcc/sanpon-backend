95ddbe5fa34930e3d466cca8a9a67ae7375f638c
DanielNery <danielpontesnery@gmail.com>
2023-11-11 19:09:10 -0300

    fix: Ajustando limite requisição suggestion.

--- app/api/api_v1/endpoints/suggestion.py
+++ app/api/api_v1/endpoints/suggestion.py
@@ -6,7 +6,7 @@ suggestion_router = APIRouter()
 
 @suggestion_router.get("/", status_code=200)
 async def retrieve_suggestion() -> BookItem:
-    return await BookItem.find_all(limit=1000).to_list()
+    return await BookItem.find_all(limit=20).to_list()
 
 
 
