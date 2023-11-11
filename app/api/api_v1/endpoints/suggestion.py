c02cf4cf6f920a1dc3171f3c49403b30503a3159
DanielNery <danielpontesnery@gmail.com>
2023-11-11 20:30:42 -0300

    feat: filtrando livros de sugestão por livros que tenham imagens.

--- app/api/api_v1/endpoints/suggestion.py
+++ app/api/api_v1/endpoints/suggestion.py
@@ -6,7 +6,7 @@ suggestion_router = APIRouter()
 
 @suggestion_router.get("/", status_code=200)
 async def retrieve_suggestion() -> BookItem:
-    return await BookItem.find_all(limit=20).to_list()
+    return await BookItem.find({"volumeInfo.imageLinks.thumbnail": {"$exists": True}}, limit=30).to_list()
 
 
 
