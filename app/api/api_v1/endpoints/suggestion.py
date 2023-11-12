618e73b13ec2bb6a7e4a21e24c11a3165e29f918
DanielNery <danielpontesnery@gmail.com>
2023-11-11 23:07:00 -0300

    feat: aleatoriedade na recomendação.

--- app/api/api_v1/endpoints/suggestion.py
+++ app/api/api_v1/endpoints/suggestion.py
@@ -1,3 +1,4 @@
+import random
 from fastapi import APIRouter
 
 from app.documents.books.book import BookItem
@@ -6,7 +7,9 @@ suggestion_router = APIRouter()
 
 @suggestion_router.get("/", status_code=200)
 async def retrieve_suggestion() -> BookItem:
-    return await BookItem.find({"volumeInfo.imageLinks.thumbnail": {"$exists": True}}, limit=30).to_list()
+    books = await BookItem.find({"volumeInfo.imageLinks.thumbnail": {"$exists": True}}, limit=60).to_list()
+    random.shuffle(books)
+    return books[:30]
 
 
 
