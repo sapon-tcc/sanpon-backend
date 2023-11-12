272dc37b23c4dd075631f08989b9e85abb2fcf6d
DanielNery <danielpontesnery@gmail.com>
2023-11-12 19:53:15 -0300

    fix: Ajustando consulta.

--- app/services/google_books.py
+++ app/services/google_books.py
@@ -5,6 +5,6 @@ import requests
 class GoogleBooksService():
     
     
-    def retrieve_books(self, q: str, s: str) -> dict:
+    def retrieve_books(self, q: str, s: str = "") -> dict:
         response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q={q}:subject:{s}&maxResults={40}&langRestrict=pt")
         return response.json()
