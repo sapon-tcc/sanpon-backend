b4d2acef2670b13429e5d4f20e39e4651d0bed7a
DanielNery <danielpontesnery@gmail.com>
2023-11-21 00:14:18 -0300

    a

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -34,8 +34,6 @@ async def get_current_user(
 async def get_current_active_user(
     current_user: user.UsersDocument = Depends(get_current_user),
 ) -> user.UsersDocument:
-    if not current_user.is_active:
-        raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
 async def get_current_active_superuser(
