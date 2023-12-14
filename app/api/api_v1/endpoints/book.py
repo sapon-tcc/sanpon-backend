a38db769625098396e209a424ad1bf395f2020b5
DanielNery <danielpontesnery@gmail.com>
2023-12-14 06:26:16 -0300

    Revert "fix: Ajustando modelo de ML."
    This reverts commit 4877bcb6eb02ca05eacfe7a7fa45c9ce7766cc7c.

--- app/api/api_v1/endpoints/book.py
+++ app/api/api_v1/endpoints/book.py
@@ -1,21 +1,27 @@
 
-import re
+
+import nltk
 import numpy as np
 import tflite_runtime.interpreter as tflite
+
+import re
+import string
 
 from fastapi import APIRouter
 from typing import List
 from pymongo.errors import DuplicateKeyError 
 from bs4 import BeautifulSoup
-
-import nltk
-nltk.download('punkt')
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
 
 from app.documents.books.book import BookItem, Opinion
 from app.services.google_books import GoogleBooksService
 
 book_router = APIRouter()
+
+nltk.download('punkt')
+nltk.download('stopwords')
 
 @book_router.get("/", status_code=200)
 async def retrieve_books(q: str= "", s: str = "") -> List[BookItem]:
@@ -43,19 +49,20 @@ async def retrieve_books(book_id: str) -> BookItem:
     return book
 
 
-def encode_words(words):
-    tokens = word_tokenize(words)
-    vocabulario = {palavra: indice for indice, palavra in enumerate(set(tokens))}
-    comentario_numerico = [vocabulario[token] for token in tokens]
-    return comentario_numerico
-    
+def encode_words(words, vocab):
+    # Mapeie palavras para índices usando uma tabela de vocabulário
+    indices = [vocab.get(word, 0) for word in words]
+    return indices
 
 @book_router.get("/opinion/{book_id}", status_code=200)
 async def retrieve_opnions_by_books(book_id: str) -> List[Opinion] :
     opnions = await Opinion.find({"book_id": book_id}).to_list()
+    vocab = {word: idx + 1 for idx, word in enumerate(set(string.punctuation))}
     interpreter = False
     for opnion in opnions:
         
+        # if opnion.classification:
+        #     continue
         
         tflite_path = './app/model/modelo.tflite'
         interpreter = tflite.Interpreter(model_path=tflite_path)
@@ -67,10 +74,12 @@ async def retrieve_opnions_by_books(book_id: str) -> List[Opinion] :
     
         text = opnion.text
         text = clean_tweet(text)
-        text_encoded = encode_words(text)
+        
+        vocab = {word: idx + 1 for idx, word in enumerate(set(text))}
+        text_encoded = encode_words(text, vocab)
         
         # Ajuste a sequência para o tamanho esperado pelo modelo
-        sequence_length = 3052
+        sequence_length = 1120
         if len(text_encoded) < sequence_length:
             # Se a sequência for menor que o esperado, preencha com zeros à direita
             text_encoded += [0] * (sequence_length - len(text_encoded))
@@ -94,7 +103,7 @@ async def retrieve_opnions_by_books(book_id: str) -> List[Opinion] :
         atualizacao = {
             "$set": {
                 "predict": opnion.predict,
-                "classification": "Positiva" if opnion.predict >= 60 else "Negativa",
+                "classification": "Positiva" if opnion.predict >= 50 else "Negativa",
             }
         }
         await opnion.update(atualizacao)
@@ -118,7 +127,15 @@ def clean_tweet(tweet):
     tweet = tweet.replace("htt", "").replace("\n", "")
     tweet = re.sub(r' +', ' ', tweet)
     
-    return tweet
+    # Tokenização usando nltk
+    words = word_tokenize(tweet)
+
+    # Remova stop words (opcional)
+    stop_words = set(stopwords.words('portuguese'))
+    words = [word for word in words if word.lower() not in stop_words and word not in string.punctuation]
+
+
+    return words
 
 
     
